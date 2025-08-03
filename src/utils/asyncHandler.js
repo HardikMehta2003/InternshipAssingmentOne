903b0a4dc0fe83dbbf91ec98aef027d0069aede2
Hardik Mehta <mehtahardik233@gmail.com>
2025-08-03 23:38:06 +0530

    feat: Add user router and utility functions for API error handling, response formatting, async handling, and Cloudinary file uploads

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+const asyncHandler = (requesrHandler)=>{
+    return (req,res,next)=>{
+        Promise.resolve(requesrHandler(req,res,next))
+        .catch((err)=>next(err))
+    }
+}
+
+
+export {asyncHandler};
+
+
+
+
+
+
+
+
+// const asyncHandler = (fn)=>async(res,req,next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code||500).json({
+//             success:false,
+//             messsge:err.messsge
+//         })
+//     }
+// }
