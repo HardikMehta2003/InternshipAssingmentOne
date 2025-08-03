dbb64f5155fb3b8d0870cb756d94ec84a1adf4fc
Hardik Mehta <mehtahardik233@gmail.com>
2025-08-04 00:56:18 +0530

    refactor: Rearrange server initialization and enhance database connection handling

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
-// src/server.js
-import { app } from './app.js';
+import dotenv from "dotenv";
+dotenv.config();
+
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to DB:", err);
+  });
